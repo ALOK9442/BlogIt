c43e22bd89c2fe4af0a0bfdddeb029a7a442152a
ALOK9442 <alok2110036@akgec.ac.in>
2023-10-22 19:38:47 +0530

    bugs fixed

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,12 @@ import { AuthLayout, Login } from './components/index.js'
 
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
 
 import AllPosts from "./pages/AllPosts";
+import Signup from './pages/SignUp.jsx'
 
 const router = createBrowserRouter([
   {
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
             path: "/signup",
             element: (
                 <AuthLayout authentication={false}>
-                    <Signup />
+                    <Signup/>
                 </AuthLayout>
             ),
         },
